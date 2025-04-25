87401a1c5036af86af2757ac5634652a4c3ebc87
Rassim Medkour <racimmedkour@gmail.com>
2025-04-25 18:13:44 +0100

    Add ImageRepository class for managing image database operations

---
+++ backend/app/repositories/image_repository.py
@@ -0,0 +1,34 @@
+"""
+ImageRepository: Handles all database operations for the Image model.
+"""
+from app.models.image import Image
+from app import db
+
+class ImageRepository:
+    @staticmethod
+    def get_by_id(image_id):
+        """Fetch an image by primary key."""
+        return Image.query.get(image_id)
+
+    @staticmethod
+    def list_by_user(user_id):
+        """List all images belonging to a user."""
+        return Image.query.filter_by(user_id=user_id).all()
+
+    @staticmethod
+    def create(image):
+        """Add a new image to the database."""
+        db.session.add(image)
+        db.session.commit()
+        return image
+
+    @staticmethod
+    def delete(image):
+        """Delete an image from the database."""
+        db.session.delete(image)
+        db.session.commit()
+
+    @staticmethod
+    def list_all():
+        """List all images."""
+        return Image.query.all()

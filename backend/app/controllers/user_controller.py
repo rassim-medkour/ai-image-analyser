766e7100be8a8741221ee46fedafbd3b959f4971
Rassim Medkour <racimmedkour@gmail.com>
2025-04-25 20:01:44 +0100

    Remove user registration controller logic from user_controller.py

--- backend/app/controllers/user_controller.py
+++ backend/app/controllers/user_controller.py
@@ -3,22 +3,3 @@ from app.services.user_service import UserService
 from app.schemas.user_schema import UserSchema
 from marshmallow import ValidationError
 
-def register_user(request_data):
-    """
-    Controller logic for user registration:
-    - Validate input using UserRegistrationSchema
-    - Call UserService.register_user
-    - Return serialized user or error message
-    """
-    schema = UserRegistrationSchema()
-    try:
-        validated_data = schema.load(request_data)
-    except ValidationError as err:
-        return {"errors": err.messages}, 400
-
-    user, error = UserService.register_user(validated_data)
-    if error:
-        return {"errors": error}, 409
-
-    user_data = UserSchema().dump(user)
-    return user_data, 201
